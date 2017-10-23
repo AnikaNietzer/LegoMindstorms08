d92f3c1dcfcc5c05a81d8f0e110d0350dcd1ad09
ByteHamster <info@bytehamster.com>
2017-10-23 11:43:45 +0200

    Renamed main file

---
+++ LegoMindstorms/src/Main.java
@@ -0,0 +1,14 @@
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+
+public class Main {
+    
+    public static void main(String[] args){
+        LCD.clear();
+        LCD.drawString("Hello World", 0, 5);
+        Button.waitForAnyPress();
+        LCD.clear();
+        LCD.refresh();
+    }
+
+}
