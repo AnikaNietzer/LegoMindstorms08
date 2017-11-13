a4d9d683c73bc3773981de0f3dd12822b72fde3d
ByteHamster <info@bytehamster.com>
2017-11-13 12:37:30 +0100

    Continuous

--- LegoMindstorms/src/edu/kit/lego08/states/bridge/BridgeForwardState.java
+++ LegoMindstorms/src/edu/kit/lego08/states/bridge/BridgeForwardState.java
@@ -1,6 +1,7 @@
 package edu.kit.lego08.states.bridge;
 
 import edu.kit.lego08.motor_control.MotorControl;
+import edu.kit.lego08.sensors.SensorUtils;
 import edu.kit.lego08.sensors.SonarService;
 import edu.kit.lego08.states.State;
 import lejos.hardware.Sound;
@@ -8,7 +9,6 @@ import lejos.hardware.lcd.LCD;
 
 public class BridgeForwardState extends State {
     private static BridgeForwardState instance = null;
-    private SonarService sonarService;
     private MotorControl motorControl = new MotorControl();
 
     private BridgeForwardState() {
@@ -25,24 +25,20 @@ public class BridgeForwardState extends State {
     @Override
     public void onEnter() {
         requestNextState(null); // Stay in current state
-        sonarService = new SonarService();
-        sonarService.start();
 
         LCD.clear();
         LCD.drawString("State: Bruecke", 0, 5);
+        motorControl.fwdRight();
     }
 
     @Override
     public void onExit() {
-        sonarService.stopService();
+        motorControl.stop(true);
     }
 
     @Override
     public void mainLoop() {
-        motorControl.forward(500);
-        sonarService.measureAll();
-
-        if (sonarService.getDistance(2) > 0.2) {
+        if (SensorUtils.getDistance() > 0.2) {
             Sound.playTone(500, 400);
             requestNextState(BridgeTurnState.getInstance());
         }
