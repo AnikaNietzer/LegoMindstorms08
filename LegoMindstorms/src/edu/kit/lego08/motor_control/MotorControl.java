ff714dace4b8d2ee0787d361a5419026fabb1565
mSchieber <uberj@student.kit.edu>
2017-11-26 16:58:41 +0100

    Use differentialPilot

--- LegoMindstorms/src/edu/kit/lego08/motor_control/MotorControl.java
+++ LegoMindstorms/src/edu/kit/lego08/motor_control/MotorControl.java
@@ -2,40 +2,36 @@ package edu.kit.lego08.motor_control;
 
 import edu.kit.lego08.motor_control.PController;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.Motor;
 import lejos.hardware.port.MotorPort;
 import lejos.robotics.RegulatedMotor;
+import lejos.robotics.navigation.DifferentialPilot;
 import lejos.utility.Delay;
 
 public class MotorControl {
 
-    private static EV3LargeRegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.A);
-    private static EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.D);
+    private static DifferentialPilot pilot = new DifferentialPilot(4.0f, 20.0f, Motor.A, Motor.D);
 
     public MotorControl() {
-        motorRight.setSpeed(360);
-        motorLeft.setSpeed(360);
+        pilot.setTravelSpeed(10);
     }
 
     public void leftTrackForward() {
         stop(true);
 
-        motorLeft.forward();
-
+        pilot.steer(100);
     }
 
     public void rightTrackForward() {
         stop(true);
 
-        motorRight.forward();
+        pilot.steer(-100);
 
     }
 
     public void turnRight(int angle) {
-        // 5.95 is factor for how much the motors have to rotate to rotate the
-        // roboter 1 degree
         stop(true);
-        motorRight.rotate(-(int) ((double) angle * 5.95), true);
-        motorLeft.rotate((int) ((double) angle * 5.95), true);
+        pilot.rotate(angle, true);
 
     }
 
@@ -43,76 +39,64 @@ public class MotorControl {
         // 5.95 is factor for how much the motors have to rotate to rotate the
         // roboter 1 degree
         stop(true);
-        motorRight.rotate((int) ((double) angle * 5.9), true);
-        motorLeft.rotate(-(int) ((double) angle * 5.9), true);
+        pilot.rotate(-angle, true);
 
     }
 
     public void turnLeftAndWait(int angle) {
-        turnLeft(angle);
-        while (isMoving()) {
-            Delay.msDelay(10);
-        }
+        stop(true);
+        pilot.rotate(-angle);
     }
 
     public void turnRightAndWait(int angle) {
-        turnRight(angle);
-        while (isMoving()) {
-            Delay.msDelay(10);
-        }
+        stop(true);
+        pilot.rotate(angle);
     }
 
     public void forward() {
         stop(true);
 
-        motorRight.forward();
-        motorLeft.forward();
-
+        pilot.forward();
     }
 
     public void forwardTimed(int millis, boolean waitForStop) {
 
         stop(true);
-        motorRight.forward();
-        motorLeft.forward();
+        pilot.forward();
         Delay.msDelay(millis);
         stop(waitForStop);
     }
 
+    public void forwardDistance(double distance) {
+        stop(true);
+        pilot.travel(distance);
+    }
+
+    public void backwardDistance(double distance) {
+        stop(true);
+        pilot.travel(-distance);
+    }
+
     public void backward() {
         stop(true);
-
-        motorRight.backward();
-        motorLeft.backward();
+        pilot.backward();
 
     }
 
     public void backwardTimed(int millis, boolean waitForStop) {
         stop(false);
-        motorRight.backward();
-        motorLeft.backward();
+        pilot.backward();
         Delay.msDelay(millis);
         stop(waitForStop);
     }
 
     public boolean isMoving() {
         Delay.msDelay(5);
-        if (motorLeft.isMoving() || motorRight.isMoving()) {
-            return true;
-        } else {
-            return false;
-        }
+        return pilot.isMoving();
     }
 
     public void stop(boolean waitForStop) {
-        if (motorLeft.isMoving() && !motorRight.isMoving()) {
-            motorLeft.stop(!waitForStop);
-        } else if (!motorLeft.isMoving() && motorRight.isMoving()) {
-            motorRight.stop(!waitForStop);
-        } else {
-            motorRight.stop(true);
-            motorLeft.stop(!waitForStop);
-        }
+        pilot.stop();
     }
 
 }
