72d5ad70d2d039d935189bbdaa58806a1659f752
ByteHamster <info@bytehamster.com>
2018-01-15 09:04:59 +0100

    Fixed forward state

--- LegoMindstorms/src/edu/kit/lego08/states/ForwardState.java
+++ LegoMindstorms/src/edu/kit/lego08/states/ForwardState.java
@@ -25,7 +25,7 @@ public class ForwardState extends State {
     public void onEnter() {
         requestNextState(null); // Stay in current state
         Button.LEDPattern(LedPattern.STATIC_YELLOW);
-        motorControl.turnLeftAndWait(15);
+        motorControl.turnLeftAndWait(10);
         motorControl.forward();
     }
 
@@ -39,7 +39,8 @@ public class ForwardState extends State {
         if (SensorUtils.getColor() == ColorEnum.BLUEMARKER) {
             requestNextState(MainMenuState.getInstance());
         } else if (SensorUtils.isTouchPressed()) {
-
+            motorControl.turnRightAndWait(10);
+            motorControl.forward();
         }
         checkEnterToMainMenu();
     }
